afec03df2b297f22007954873af9c094ba413036
Abdel bendouil <ccrashbandicot@hotmail.fr>
2016-08-21 20:23:33 +0200

    Update us-proxy.pl

--- us-proxy.pl
+++ us-proxy.pl
@@ -8,15 +8,17 @@ system $^O eq 'MSWin32' ? 'cls' : 'clear';
 
 print "\t      *****************\n\t      us-proxy.org Grabber\n\t      by CrashBandicot\n\t      *****************\n\n\n";
 
-print "\n";
+my $ua = LWP::UserAgent->new();
 
-	 $ua = LWP::UserAgent->new();
-	 $ua->agent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-         $res = $ua->get("https://www.us-proxy.org/")->content;
+$ua->agent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
 
-        while($res =~ m/<tr><td>(.*?)<\/td><td>(.*?)<\/td>/g) {
-        	$ip = $1;
-                $port = $2;
+my $req = $ua->get("http://us-proxy.org/")->content;
+
+while($req =~ m/<tr><td>(.*?)<\/td><td>(.*?)<\/td>/g) {
+
+
+       	$ip = $1;
+        $port = $2;
 
         $zebi = $ip.":".$port."\n";
        print $zebi;
@@ -24,6 +26,4 @@ print "\n";
        open(SAVE , ">>proxxy.txt");
        print SAVE $zebi;
        close(SAVE);
-        }
- 
-__END__
+           }
